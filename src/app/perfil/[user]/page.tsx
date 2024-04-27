15fe57626b900f88bc0362e2e7213d6ace4a3f65
Alisson Oliveira <binho_alisson@hotmail.com>
2024-04-27 19:02:07 -0300

    fix: error with generateStaticParams on foto/[id] page

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -6,6 +6,13 @@ interface ParamsProps {
   }
 }
 
+export async function generateStaticParams() {
+  return [
+    { curso: 'html' },
+    { curso: 'css' },
+  ];
+}
+
 export default async function UserPage({ params }: ParamsProps) {
   return (
     <main>UserPage: {params.user}</main>
