83203e1501201fb8d35d9c93706b1426840d1f62
Alisson Oliveira <binho_alisson@hotmail.com>
2024-04-27 18:31:30 -0300

    fix: error with generateStaticParams on foto/[id] page

--- src/app/foto/[id]/page.tsx
+++ src/app/foto/[id]/page.tsx
@@ -1,3 +1,5 @@
+import photosGet from '@/actions/photos-get';
+import Image from 'next/image';
 import React from 'react'
 
 interface ParamsProps {
@@ -6,9 +8,29 @@ interface ParamsProps {
   }
 }
 
+export async function generateStaticParams() {
+  const data = await photosGet();
+  return data.map((photo) => ({
+    id: photo.id,
+  }));
+}
+
 export default async function FotoIdPage({ params }: ParamsProps) {
+  const data = await photosGet();
 
   return (
-    <main>{params.id}</main>
+    <section>
+      {data?.map((photo) => (
+        <div key={photo.id}>
+          <Image
+            src={photo.src}
+            alt={photo.title}
+            width={1500}
+            height={1500}
+            sizes='80vw'
+          />
+        </div>
+      ))}
+    </section>
   )
 }
