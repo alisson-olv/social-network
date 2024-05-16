46522f7d417db3d1c5b949bfdb868870fdc0e5ca
Alisson <binho_alisson@hotmail.com>
2024-05-15 22:00:42 -0300

    fix: resolve generateStaticParams

--- src/app/foto/[id]/page.tsx
+++ src/app/foto/[id]/page.tsx
@@ -1,4 +1,5 @@
 import photoGet from '@/actions/photo-get';
+import photosGet from '@/actions/photos-get';
 import PhotoContent from '@/components/photo/photo-content';
 import { notFound } from 'next/navigation';
 import React from 'react';
@@ -18,12 +19,12 @@ export async function generateMetadata({ params }: PhotoIdParams) {
   }
 }
 
-// export async function generateStaticParams() {
-//   const { data } = await photosGet();
-//   return data?.map((photo) => ({
-//     id: `${ photo.id } `,
-//   }));
-// }
+export async function generateStaticParams() {
+  const { data } = await photosGet();
+  return data?.map((photo) => ({
+    id: String(photo.id),
+  }));
+}
 
 export default async function FotoIdPage({ params }: PhotoIdParams) {
   const { data } = await photoGet(params.id);
